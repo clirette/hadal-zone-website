30a79891057512c3a8b0e8c6fdd7d0fa350a873a
Chase Lirette <chaselirette@yahoo.com>
2017-06-04 17:57:19 -0500

    stashing commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showIFrame() {  
+	
+	var iframe = document.getElementById("forOpenIFrame");  
+	iframe.style.display="block";  
+
+	var content = document.getElementById("content");
+	content.style.display="none";
+}
