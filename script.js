644808ac67c84320c1f12651acff37250e44125b
Chase Lirette <chaselirette@yahoo.com>
2017-06-07 14:27:42 -0500

    Added about page, changed buttons to links styled as bootstrap buttons, deleted script.js, changed some styles a bit

--- script.js
+++
@@ -1,8 +0,0 @@
-function showIFrame() {  
-	
-	var iframe = document.getElementById("forOpenIFrame");  
-	iframe.style.display="block";  
-
-	var content = document.getElementById("content");
-	content.style.display="none";
-}
